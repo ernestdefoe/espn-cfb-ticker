8ba17a5e690dfb06b163ab83f684904757decf75
ernestdefoe <ernestdefoe@gmail.com>
2026-05-09 09:06:52 -0500

    fix correct webpack build command and deps

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -2,4 +2,10 @@ const config = require('flarum-webpack-config');
 
 module.exports = config({
     useExtensions: [],
+    // Mithril JSX: transform m() calls via babel
+    babelOptions: {
+        plugins: [
+            ['@babel/plugin-transform-react-jsx', { pragma: 'm' }]
+        ]
+    }
 });
